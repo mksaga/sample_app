57fbb2478e6a6f066a5b6fc12371c5356d53050b
Mohamed <mohamed.aly06@gmail.com>
2015-12-14 17:12:34 +0000

    Finish static pages with layout file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  def welcome
-    render text: "welcome to Heroku."
-  end
-  
 end
