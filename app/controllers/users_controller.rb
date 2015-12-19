f8fbef28b8303fce43512cc18436c88cb82582d7
Mohamed <mohamed.aly06@gmail.com>
2015-12-19 21:40:21 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,32 @@
 class UsersController < ApplicationController
+  
   def new
+    @user = User.new
   end
+  
+  def show
+    @user = User.find_by(params[:id])
+    # if 'debugger' below is uncommented, rails server shows a console
+    # that allows checking which user is displaying, their properties, etc.
+    # debugger
+  end
+  
+  def create
+    # using "user_params" restricts what can be passed into signup form
+    @user = User.new(user_params) 
+    if @user.save 
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, 
+                                   :password_confirmation)
+    end
+  
 end
